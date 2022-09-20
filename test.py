a4afc9868463362545422984dce4156248bdc51c
Bitjockey <fastjack@jackpoint.network>
2022-09-20 18:49:58 -0400

    Account for simple case

--- test.py
+++ test.py
@@ -4,8 +4,13 @@ import util
 
 
 class TestTraversalMethod(unittest.TestCase):
-    def test_traverse_simple(self):
+    def test_empty(self):
         self.assertEqual(util.traverse({}), {})
+    
+    def test_simple(self):
+        data = {"name": "E.M. Forster"}
+        expected = {"name": str}
+        self.assertEqual(util.traverse(data), expected)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -2,4 +2,10 @@ from typing import Dict
 
 
 def traverse(data: Dict):
-    return data
+    """Traverse data and determine types"""
+    traversed = {}
+
+    for k, v in data.items():
+        traversed[k] = type(v)
+
+    return traversed
