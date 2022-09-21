64a58fe494b365e15c6b62c8b53743184b8f4d41
Bitjockey <bitjockey@jackpoint.network>
2022-09-20 21:34:34 -0400

    Convert to pytest

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,3 @@
-import unittest
-
 from datetime import datetime
 
 from mock_server import util
@@ -10,61 +8,64 @@ class FakeDataType:
         pass
 
 
-class TestTraversalMethod(unittest.TestCase):
-    def test_empty(self):
-        self.assertEqual(util.traverse({}), {})
-    
-    def test_simple(self):
-        data = {
-            "author": "E.M. Forster",
-            "age": 48,
-        }
-        expected = {"author": str, "age": int}
-        self.assertEqual(util.traverse(data), expected)
+def test_traverse_empty():
+    assert util.traverse({}) == {}
 
-    def test_nested(self):
-        data = {
-            "author": "E.M. Forster",
-            "age": 48,
-            "metadata": {
-                "code": "abc1234",
-                "data": {
-                    "number": "twelve"
-                }
+
+def test_traverse_simple():
+    data = {
+        "author": "E.M. Forster",
+        "age": 48,
+    }
+    expected = {"author": str, "age": int}
+    assert util.traverse(data) == expected
+
+
+def test_traverse_nested():
+    data = {
+        "author": "E.M. Forster",
+        "age": 48,
+        "metadata": {
+            "code": "abc1234",
+            "data": {
+                "number": "twelve"
             }
         }
-        expected = {
-            "author": str,
-            "age": int,
-            "metadata": {
-                "code": str,
-                "data": {
-                    "number": str
-                }
+    }
+    expected = {
+        "author": str,
+        "age": int,
+        "metadata": {
+            "code": str,
+            "data": {
+                "number": str
             }
         }
-        self.assertEqual(util.traverse(data), expected)
+    }
+    assert util.traverse(data) == expected
 
-    def test_datetime(self):
-        data = {
-            "name": "Maurice Hall",
-            "dob": datetime(1898, 10, 1)
-        }
-        expected = {
-            "name": str,
-            "dob": datetime
-        }
-        self.assertEqual(util.traverse(data), expected)
 
-    def test_custom_datatype(self):
-        data = {
-            "name": "Maurice Hall",
-            "dob": datetime(1898, 10, 1),
-            "fakedata": FakeDataType()
-        }
-        expected = {
-            "name": str,
-            "dob": datetime,
-            "fakedata": FakeDataType
-        }
-        self.assertEqual(util.traverse(data), expected)
+def test_traverse_datetime():
+    data = {
+        "name": "Maurice Hall",
+        "dob": datetime(1898, 10, 1)
+    }
+    expected = {
+        "name": str,
+        "dob": datetime
+    }
+    assert util.traverse(data) == expected
+
+
+def test_traverse_custom_datatype():
+    data = {
+        "name": "Maurice Hall",
+        "dob": datetime(1898, 10, 1),
+        "fakedata": FakeDataType()
+    }
+    expected = {
+        "name": str,
+        "dob": datetime,
+        "fakedata": FakeDataType
+    }
+    assert util.traverse(data) == expected
