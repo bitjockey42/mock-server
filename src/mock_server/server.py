4f70ad7f07de1673b1de41e04bd325dfa9272717
Al Julian <ajulian@theatlantic.com>
2022-11-11 15:32:59 -0500

    Print callback path

--- src/mock_server/server.py
+++ src/mock_server/server.py
@@ -18,9 +18,10 @@ def callback(resource, endpoint):  # this could also be dynamically generated
     response = request_data
     response["query_strings"] = request.query_string
     response["query"] = dict(request.query)
+    # response.headers['Content-Type'] = 'xml/application'
     return response
 
 
 def start_server(host, port, debug):
-    print(callback_path)
+    print("callback_path", callback_path)
     run(host=host, port=port, debug=debug)
