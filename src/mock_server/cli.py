973f58c5eec88defbcb01c6f4a674d36583c0805
Al Julian <ajulian@theatlantic.com>
2022-11-11 15:44:57 -0500

    Add flask

--- src/mock_server/cli.py
+++ src/mock_server/cli.py
@@ -1,5 +1,6 @@
 import click
 
+from mock_server.app import start_app
 from mock_server.server import start_server
 from mock_server.util import generate_structure_from_file, generate_data_from_file
 
@@ -16,6 +17,14 @@ def cli():
 @click.option("-D", "--debug/--no-debug", default=False)
 def start(host, port, debug):
     start_server(host, port, debug)
+
+
+@cli.command()
+@click.option("-H", "--host", default="localhost")
+@click.option("-P", "--port", default=8080)
+@click.option("-D", "--debug/--no-debug", default=False)
+def app(host, port, debug):
+    start_app(host, port, debug)
 
 
 @cli.command()
