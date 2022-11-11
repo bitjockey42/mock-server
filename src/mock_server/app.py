79227a5bb574c208a0fa558659046a14df77fe30
Al Julian <ajulian@theatlantic.com>
2022-11-11 16:33:09 -0500

    Return xml

--- src/mock_server/app.py
+++ src/mock_server/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request
 from pathlib import Path
+
+from flask import Flask, request
+from flask_restful import Api
+
+from json2xml import json2xml
+from json2xml.utils import readfromjson
 
 from mock_server.util import read_json
 
 ROOT_DIR = Path(__file__).parent.parent.parent
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route("/")
@@ -13,9 +19,12 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@api.representation("application/xml")
 @app.route("/<path:subpath>", methods=["POST"])
 def callback(subpath):
-    response = read_json(ROOT_DIR.joinpath("tmp", "test.json"))
+    json_filename = ROOT_DIR.joinpath("tmp", "test.json")
+    data = readfromjson(json_filename)
+    response = json2xml.Json2xml(data).to_xml()
     return response
 
 
