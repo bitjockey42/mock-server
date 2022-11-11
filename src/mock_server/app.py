75a22eddee684e3b76aa61bf10e76bb5e3a29d40
Al Julian <ajulian@theatlantic.com>
2022-11-11 15:48:54 -0500

    Include subpath

--- src/mock_server/app.py
+++ src/mock_server/app.py
@@ -9,5 +9,10 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/<path:subpath>", methods=["POST"])
+def callback(subpath):
+    return f"{subpath}"
+
+
 def start_app(host, port, debug):
     app.run(host=host, port=port, debug=debug)
