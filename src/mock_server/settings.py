957fcf29c99bebc7b632c9ff8898c6b19e312b48
Al Julian <ajulian@theatlantic.com>
2022-11-30 10:18:52 -0500

    Don't create dir

--- src/mock_server/settings.py
+++ src/mock_server/settings.py
@@ -9,10 +9,6 @@ load_dotenv()
 BASE_DIR = Path().cwd()
 DEFAULT_DATA_DIR = BASE_DIR.joinpath("data")
 DATA_DIR = Path(os.getenv("DATA_DIR", DEFAULT_DATA_DIR)).resolve()
-
-if not DATA_DIR.exists():
-    DATA_DIR.mkdir(parents=True)
-
 BASE_API_PATH = os.getenv("BASE_API_PATH", "")
 DATA_STRATEGY = os.getenv("DATA_STRATEGY", "from_request")
 DEFAULT_DATA_FORMAT = os.getenv("DEFAULT_DATA_FORMAT", "json")
