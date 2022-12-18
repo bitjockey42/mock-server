dcab5682393c18541c0127433965af886986a4b2
Bitjockey <fastjack@jackpoint.network>
2022-12-18 09:34:50 -0500

    Set to json

--- src/mock_server/settings.py
+++ src/mock_server/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_API_PATH = os.getenv("BASE_API_PATH", "")
 BASE_DIR = Path().cwd()
 
-DEFAULT_DATA_FORMAT = os.getenv("DEFAULT_DATA_FORMAT", "xml")
+DEFAULT_DATA_FORMAT = os.getenv("DEFAULT_DATA_FORMAT", "json")
 
 DEFAULT_CONF_DIR = BASE_DIR.joinpath("conf")
 CONF_DIR = Path(os.getenv("CONF_DIR", DEFAULT_CONF_DIR)).resolve()
